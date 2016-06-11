4f0ea45a2e4a1932ac74f5c624767c111f0145d9
Lenny Leithauser <lleithauser@ufl.edu>
2016-06-10 23:33:10 -0400

    updated to add functions- howdy, numba, and inception (which I couldn't figure out)

--- js/main.js
+++ js/main.js
@@ -1,19 +1,28 @@
-//function declaration howdy
-
+//fPart A- function declaration howdy
 function howdy( person, message ) {
     return person + ' says ' + message;
 }
 
-var message = combiner( 'Lenny Leithauser', 'hi!' );
+var message = howdy( 'Lenny Leithauser', 'hi!' );
 console.log( message );
 
-//function expression numba
-var numba = 9;
+//Part B- function expression numba
+function numba(string) {
 
-if ( 30 < numberToTest ) {
-    console.log( 'numberToTest is greater than 30.' );
-} else if ( numberToTest == 17 ) {
-    console.log( 'numberToTest is 17.' );
-} else {
-    console.log( 'numberToTest is not greater than 30 and is not the number 17.' );
+if ( 7 > string.length ) {
+    console.log( 'What a short little word!' );
+} else if ( 7 < string.length ) {
+    console.log( "I'm sorry, but that's too many to count." );
+} else if ( string.length == 7 ) {
+    console.log( '7, what a perfect choice!' );
 }
+};
+numba ('7')
+numba ('seventy')
+numba ('university')
+
+//Part C- function inception.. I CANNOT figure this out
+function inception(display, favMovie) {
+    display(favMovie);
+};
+console.log(favMovie + 'is a fantastic movie.')
