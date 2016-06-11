04ad428b9c8cf13f4d9441310da455e2a4dde6fc
Lenny Leithauser <lleithauser@ufl.edu>
2016-06-10 22:05:11 -0400

    finally fixed the president code (I think)

--- js/main.js
+++ js/main.js
@@ -1,44 +1,43 @@
-//array
 var presidents = {
-    fullName: "List of Presidents",
 
 //objects
-presidents: [
+presidentList: [
 {
     firstName: 'Barack',
     lastName: 'Obama',
-    termLength: '2',
+    termLength: 2,
     party: 'Democrat',
-    yearsOfPresidency: '8'
+    yearsOfPresidency: 8
 },
 {
     firstName: 'George W',
     lastName: 'Bush',
-    termLength: '2',
+    termLength: 2,
     party: 'Republican',
-    yearsOfPresidency: '8'
+    yearsOfPresidency: 8
 },
 {
     firstName: 'Bill',
     lastName: 'Clinton',
-    termLength: '1',
+    termLength: 1,
     party: 'Democrat',
-    yearsOfPresidency: '4'
+    yearsOfPresidency: 4
 },
 {
     firstName: 'George H W',
     lastName: 'Bush',
-    termLength: '1',
+    termLength: 1,
     party: 'Republican',
-    yearsOfPresidency: '4'
+    yearsOfPresidency: 4
 },
 {
     firstName: 'Ronald',
     lastName: 'Reagan',
-    termLength: '2',
+    termLength: 2,
     party: 'Republican',
-    yearsOfPresidency: '8'
+    yearsOfPresidency: 8
 }
 ]
-}
-console.log( presidents.[3].firstName presidents.[3].lastName );
+};
+
+console.log( presidents.presidentList[2].firstName + ' ' + presidents.presidentList[2].lastName );
