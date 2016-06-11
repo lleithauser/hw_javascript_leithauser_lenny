30d8886f0ceeeb788a7db66e98c47fab2277e0a2
Lenny Leithauser <lleithauser@ufl.edu>
2016-06-10 21:13:54 -0400

    updated main.js to add president data

---
+++ js/main.js
@@ -0,0 +1,44 @@
+//array
+var presidents = {
+    fullName: "List of Presidents",
+
+//objects
+presidents: [
+{
+    firstName: 'Barack',
+    lastName: 'Obama',
+    termLength: '2',
+    party: 'Democrat',
+    yearsOfPresidency: '8'
+},
+{
+    firstName: 'George W',
+    lastName: 'Bush',
+    termLength: '2',
+    party: 'Republican',
+    yearsOfPresidency: '8'
+},
+{
+    firstName: 'Bill',
+    lastName: 'Clinton',
+    termLength: '1',
+    party: 'Democrat',
+    yearsOfPresidency: '4'
+},
+{
+    firstName: 'George H W',
+    lastName: 'Bush',
+    termLength: '1',
+    party: 'Republican',
+    yearsOfPresidency: '4'
+},
+{
+    firstName: 'Ronald',
+    lastName: 'Reagan',
+    termLength: '2',
+    party: 'Republican',
+    yearsOfPresidency: '8'
+}
+]
+}
+console.log( presidents.[3].firstName presidents.[3].lastName );
