994d2e98cf184fd87f14cfcda60f2a88bb04e000
Lenny Leithauser <lleithauser@ufl.edu>
2016-06-10 21:26:15 -0400

    first attempt at functions branch

---
+++ js/main.js
@@ -0,0 +1,19 @@
+//function declaration howdy
+
+function howdy( person, message ) {
+    return person + ' says ' + message;
+}
+
+var message = combiner( 'Lenny Leithauser', 'hi!' );
+console.log( message );
+
+//function expression numba
+var numba = 9;
+
+if ( 30 < numberToTest ) {
+    console.log( 'numberToTest is greater than 30.' );
+} else if ( numberToTest == 17 ) {
+    console.log( 'numberToTest is 17.' );
+} else {
+    console.log( 'numberToTest is not greater than 30 and is not the number 17.' );
+}
